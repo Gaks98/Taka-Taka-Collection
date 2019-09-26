7fa17a336cc001fdcd86ebc7fbe421d04fc309cd
komen225 <komen.robert19@gmail.com>
2019-09-26 06:12:38 +0300

    add override method to store new instance of the estate.

--- src/main/java/models/Estate.java
+++ src/main/java/models/Estate.java
@@ -1,12 +1,16 @@
 package models;
 
+import java.util.Objects;
+
 public class Estate {
     private int id;
     private String name;
     private String location;
     private int collector_id;
 
-  public Estate(String name,String location,int collector_id){
+
+
+    public Estate(String name, String location, int collector_id){
       this.name=name;
       this.location=location;
       this.collector_id=collector_id;
@@ -25,5 +29,22 @@ public class Estate {
 
     public int getE_collector_id() {
       return collector_id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)return true;
+        if(o==null||getClass()!=o.getClass())return false;
+
+        Estate estate =(Estate)o;
+        return getE_collector_id()== estate.getE_collector_id()&&
+                getE_name().equals(estate.getE_name()) &&
+                getE_location().equals(estate.getE_location());
+    }
+    @Override
+    public int hashCode() {
+
+            return Objects.hash(getE_name(), getE_name(), getE_collector_id());
+
     }
 }
