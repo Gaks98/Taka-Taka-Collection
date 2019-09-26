0ba990e1a7d83aea8a163691d8a6e95bcc85a6d8
komen225 <komen.robert19@gmail.com>
2019-09-26 06:24:11 +0300

    Add impimentation of the ride through the class Estate on its interface to the class.

--- src/main/java/dao/Sql2oEsateDao.java
+++ src/main/java/dao/Sql2oEsateDao.java
@@ -1,4 +1,32 @@
 package dao;
 
-public class Sql2oEsateDao {
+import models.Estate;
+
+import java.util.List;
+
+public class Sql2oEsateDao implements EstateDao {
+    @Override
+    public void add(Estate estate) {
+
+    }
+
+    @Override
+    public Estate findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Estate> getAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
