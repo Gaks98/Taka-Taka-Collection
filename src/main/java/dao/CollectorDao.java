93a50947f3a7770921281a7486cd0d1cd724cfc1
Franklin-Kimatu <frankimatu@gmail.com>
2019-09-26 08:32:00 +0300

    add the override functionality methods

--- src/main/java/dao/CollectorDao.java
+++ src/main/java/dao/CollectorDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface CollectorDao {
     //create
-    void add(Collector collector);
+    void add (Collector collector);
 
     //read
     List<Collector>getAll();
